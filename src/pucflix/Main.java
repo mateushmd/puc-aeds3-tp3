338b2c9ae165616dbcb88f4c6e25cac07ec00e03
mateushmd <mateushmdiniz@gmail.com>
2025-05-26 21:41:18 -0300

    adding on postings list

--- src/pucflix/Main.java
+++ src/pucflix/Main.java
@@ -1,14 +1,26 @@
 package pucflix;
 
 import pucflix.view.*;
+import pucflix.model.*;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+        /*
         try
         {
             Prompt prompt = new Prompt();
             prompt.start();
             prompt.close();
         } catch(Exception ex) { System.err.println(ex.getMessage()); }
+        */
+        try {
+        File f = new File("./dados");
+        if(!f.exists())
+            f.mkdir();
+        PostingsList l = new PostingsList(4, "./dados/dicionario.listaenv.bd", "./dados/blocos.listaenv.bd");
+
+        l.add("Três tigres com três pratos para três tigres", 123);
+        } catch(Exception ex){ System.err.println(ex.getMessage());}
     }
 }
